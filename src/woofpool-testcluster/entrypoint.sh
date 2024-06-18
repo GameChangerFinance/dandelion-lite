aecf32ff3b30a0a8f6ce4126c355a53521975ea9
Maarten Menheere <95027689+M2tec@users.noreply.github.com>
2024-06-18 13:48:21 +0200

    Make it work on podman

--- src/woofpool-testcluster/entrypoint.sh
+++ src/woofpool-testcluster/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 git clone -b geniusyield --single-branch https://github.com/M2tec/cardano-private-testnet-setup.git /testcluster
-cd /testcluster && ./scripts/automate.sh
+
+# The 1 will make sure the testnet configuration is not deleted on restart
+cd /testcluster && ./scripts/automate.sh 1
