481e79f50452f1759ea8e4ce72e11e17bdbd9122
Maarten Menheere <m.menheere@m2tec.nl>
2025-03-25 12:31:49 +0100

    export as json block reads

--- tools/stats_block_read.sql
+++ tools/stats_block_read.sql
@@ -1,9 +1,12 @@
-SELECT
-    calls,
-    shared_blks_hit,
-    shared_blks_read,
-    (shared_blks_read + shared_blks_hit) AS total_blocks,
-    query
-FROM pg_stat_statements
-ORDER BY shared_blks_read DESC
-LIMIT 10;
+SELECT json_agg(t)
+FROM (
+    SELECT
+        calls,
+        shared_blks_hit,
+        shared_blks_read,
+        (shared_blks_read + shared_blks_hit) AS total_blocks,
+        query
+    FROM pg_stat_statements
+    ORDER BY shared_blks_read DESC
+    LIMIT 10
+) t;
