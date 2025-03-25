fe0f7689dc315ef9046a12a4e0b398f553ee64a6
Maarten Menheere <m.menheere@m2tec.nl>
2025-03-25 12:21:51 +0100

    Add block read query

---
+++ tools/stats_block_read.sql
@@ -0,0 +1,9 @@
+SELECT
+    calls,
+    shared_blks_hit,
+    shared_blks_read,
+    (shared_blks_read + shared_blks_hit) AS total_blocks,
+    query
+FROM pg_stat_statements
+ORDER BY shared_blks_read DESC
+LIMIT 10;
