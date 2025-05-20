b65519e30cf7b9b5fd39b5df30915608273e61c6
M2tec <info@m2tec.nl>
2025-05-20 16:54:01 +0200

    Add network var

--- scripts/ssl/setup_nginx.sh
+++ scripts/ssl/setup_nginx.sh
@@ -9,44 +9,25 @@ echo ${DUCKDNS_DOMAINS}.duckdns.org
 
 DOMAIN=${DUCKDNS_DOMAINS}.duckdns.org
 
-cat <<EOF | sudo tee /etc/nginx/sites-available/dandelion  > /dev/null
+cat <<EOF | sudo tee /etc/nginx/sites-available/dandelion-${NETWORK}  > /dev/null
 server {
     listen 80;
     listen [::]:80;
 
-    server_name preprod.${DOMAIN};
+    server_name ${NETWORK}.${DOMAIN};
 
     location / {
-        proxy_pass http://localhost:3081/;
+        proxy_pass http://localhost:${HAPROXY_PORT}/;
         proxy_http_version 1.1;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
-
-}
-
-server {
-    listen 80;
-    listen [::]:80;
-
-    server_name mainnet.${DOMAIN};
-
-    location / {
-        proxy_pass http://localhost:3080/;
-        proxy_http_version 1.1;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-    }
-
 }
 EOF
 
-
-sudo ln -sf /etc/nginx/sites-available/dandelion /etc/nginx/sites-enabled/
+sudo ln -sf /etc/nginx/sites-available/dandelion-${NETWORK} /etc/nginx/sites-enabled/
 
 sudo nginx -t && sudo systemctl reload nginx
 
