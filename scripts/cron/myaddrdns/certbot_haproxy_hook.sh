aad4431a18e7eb5b637d0d6403869b0b5659fdc6
M2tec <info@m2tec.nl>
2025-10-16 16:17:18 +0200

    Fix server.pem as folder

--- scripts/cron/myaddrdns/certbot_haproxy_hook.sh
+++ scripts/cron/myaddrdns/certbot_haproxy_hook.sh
@@ -3,10 +3,11 @@
 set -e
 
 CERT_DIR="/etc/letsencrypt/live/${MYADDR_DOMAIN}.myaddr.io"
-PEM_OUTPUT="/etc/letsencrypt/server.pem"
+PEM_OUTPUT="/data/ssl/server.pem"
 
 echo "Setting up Haproxy keys for ${MYADDR_DOMAIN}.myaddr.io"
- 
+echo "SSL folder: ${CERT_DIR}"
+
 if [ -f "$CERT_DIR/privkey.pem" ] && [ -f "$CERT_DIR/fullchain.pem" ]; then
     cat "$CERT_DIR/privkey.pem" "$CERT_DIR/fullchain.pem" > "$PEM_OUTPUT"
 else
