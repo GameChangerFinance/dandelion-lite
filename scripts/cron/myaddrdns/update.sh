9005b0273d24b12bd7e1e753bb41873d3ee21e5f
zxpectre <15870846+zxpectre@users.noreply.github.com>
2025-12-02 16:12:00 +0000

    fix: removed unused env var and cleaned up logic for myaddr service

--- scripts/cron/myaddrdns/update.sh
+++ scripts/cron/myaddrdns/update.sh
@@ -15,14 +15,11 @@ UPDATE_URL="https://myaddr.io/update"
 # Log update attempt
 echo "[UPDATE] $(date -u)"
 
-echo "IPv6: ${MYADDR_IP_V6_ENABLED}"
-
-if [[ -n ${MYADDR_IP_V6_ENABLED} ]]; then
-  CURRENT_IP=`cat /scripts/cron/myaddrdns/my_ip_v6.txt`
-  IP_TO_USE=${CURRENT_IP}
+if [[ -n ${MYADDR_IP_V6_ENABLED} ]]; then  
   UPDATE_URL="https://ipv6.myaddr.io/update"
+  echo "Using IPv6. (unset MYADDR_IP_V6_ENABLED= to use IPv4)"
 else
-  CURRENT_IP=$(curl -s https://api.ipify.org)
+  echo "Using IPv4. (set MYADDR_IP_V6_ENABLED=true to use IPv6)"  
 fi
 
 
@@ -30,9 +27,9 @@ fi
 #CURRENT_IP=$(curl -s https://icanhazip.com)
 
 if [[ -n ${MYADDR_DOMAIN} ]]; then
-  echo "Setting '${MYADDR_DOMAIN}.myaddr.<tools|dev|io>' IP address to be '$CURRENT_IP' ..."
+  echo "Setting '${MYADDR_DOMAIN}.myaddr.<tools|dev|io>' IP address to be '$IP_TO_USE' ..."
 else
-  echo "Setting your '<DOMAIN>.myaddr.<tools|dev|io>' domain IP address to be '$CURRENT_IP' ..."
+  echo "Setting your '<DOMAIN>.myaddr.<tools|dev|io>' domain IP address to be '$IP_TO_USE' ..."
 fi
 
 # Build request payload
@@ -41,6 +38,7 @@ POST_DATA="key=${MYADDR_TOKEN}&ip=${IP_TO_USE}"
 # Include ACME challenge if set
 if [[ -n ${MYADDR_ACME_CHALLENGE} ]]; then
   POST_DATA="${POST_DATA}&acme_challenge=${MYADDR_ACME_CHALLENGE}"
+  echo "Using ACME challenge"
 fi
 
 echo "curl -s -X POST -d "${POST_DATA}" "${UPDATE_URL}""
